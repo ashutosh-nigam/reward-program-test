cdfbdf2ab6be23f35a994e9c1ef4f3c6a45c56e9
ashutosh.nigam <ashutosh.nigam@live.com>
2022-05-23 23:21:35 +0530

    Added ProductsController.cs for product Information

---
+++ RewardProgramAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RewardProgramAPI.Data;
+
+namespace RewardProgramAPI.Controllers;
+
+/// <summary>
+/// Products Information
+/// </summary>
+[ApiController]
+[Route("products")]
+public class ProductsController : ControllerBase
+{
+    private readonly RewardProgramDbContext _context;
+
+    public ProductsController(RewardProgramDbContext context)
+    {
+        _context = context;
+    }
+    /// <summary>
+    /// Get List of All Products and their details
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType( typeof(IEnumerable<ViewModels.Product>),StatusCodes.Status200OK)]
+    [Produces("application/json")]
+    public IEnumerable<ViewModels.Product> GetAll()
+    {
+        return _context.Products.Select(x => new ViewModels.Product()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Price = x.Price
+        });
+    }
+}
